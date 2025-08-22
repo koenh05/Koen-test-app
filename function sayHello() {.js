07d7611285bdce0e8cb3e0266efb9e83516b3251
koenh05 <koen.harink@outlook.com>
2025-08-22 10:45:45 +0200

    Add files via upload

--- function sayHello() {.js
+++ function sayHello() {.js
@@ -1,3 +1,23 @@
 function sayHello() {
     alert("Hallo! Dit is mijn eerste app 🚀");
 }
+function voegToe() {
+  const input = document.getElementById("product");
+  const lijst = document.getElementById("lijst");
+
+  if (input.value.trim() === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = input.value;
+  
+  // verwijderknop
+  const btn = document.createElement("button");
+  btn.textContent = "❌";
+  btn.style.marginLeft = "10px";
+  btn.onclick = () => li.remove();
+
+  li.appendChild(btn);
+  lijst.appendChild(li);
+
+  input.value = "";
+}
