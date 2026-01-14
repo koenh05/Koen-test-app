2397bdbd3e4ebdcdc128f92c00c1847122264502
koenh05 <koen.harink@outlook.com>
2026-01-14 16:07:47 +0100

    Update sayHello function alert message

--- function sayHello() {.js
+++ function sayHello() {.js
@@ -1,23 +1,3 @@
 function sayHello() {
-    alert("Hallo! Dit is mijn eerste app 🚀");
-}
-function voegToe() {
-  const input = document.getElementById("bestellingen");
-  const lijst = document.getElementById("lijst");
-
-  if (input.value.trim() === "") return;
-
-  const li = document.createElement("li");
-  li.textContent = input.value;
-  
-  // verwijderknop
-  const btn = document.createElement("button");
-  btn.textContent = "❌";
-  btn.style.marginLeft = "10px";
-  btn.onclick = () => li.remove();
-
-  li.appendChild(btn);
-  lijst.appendChild(li);
-
-  input.value = "";
+    alert("Hallo, dit is een test!");
 }
